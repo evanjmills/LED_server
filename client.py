b6294ccf3681a2cdc3b9ca9e069ce8d2815317bb
Evan <evanmills113@gmail.com>
2020-10-26 18:15:41 -0500

    multiple server client

--- client.py
+++ client.py
@@ -10,33 +10,38 @@ def main():
         
 
 async def set_lights():
-    uri = 'ws://192.168.0.132:8080'
+    uris = ['ws://192.168.0.132:8080', 'ws://192.168.0.145:8080']
+    ws = []
 
     print('Connecting the websocket...')
+    for uri in uris:
+        ws.append(await websockets.connect(uri))
+    
+    print('Client Started')
 
-    async with websockets.connect(uri) as websocket:
-        print('Client Started')
-        while True:
-            try:
-                root = tk.Tk()
-                style = ttk.Style(root)
-                style.theme_use('clam')
+    while True:
+        try:
+            root = tk.Tk()
+            style = ttk.Style(root)
+            style.theme_use('clam')
 
-                temp = askcolor()[0]
-                r = temp[0]
-                g = temp[1]
-                b = temp[2]
-                root.destroy()
+            temp = askcolor()[0]
+            r = temp[0]
+            g = temp[1]
+            b = temp[2]
+            root.destroy()
 
-                rgb = f'{r},{g},{b}'
+            rgb = f'{r},{g},{b}'
 
+            for websocket in ws:
                 await websocket.send(rgb)
-                print(f'Sent: {rgb}')
+    
+            print(f'Sent: {rgb}')
 
-                response = await websocket.recv()
-                print(f'Recieved: {response}')
-            except websockets.exceptions.ConnectionClosed:
-                break
+            response = await websocket.recv()
+            print(f'Recieved: {response}')
+        except websockets.exceptions.ConnectionClosed:
+            break
             
 
 if __name__ == '__main__':
