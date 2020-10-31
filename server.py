f7af98b1a3e5f717e118eea1a403ae516ea0bf13
Evan <evanmills113@gmail.com>
2020-10-30 20:01:41 -0500

    updates

--- server.py
+++ server.py
@@ -18,69 +18,56 @@ class Server:
         self.preset_thread = None
         self.num_leds = 190
         self.pixels = neopixel.NeoPixel(board.D18, self.num_leds, brightness=0.1, auto_write=False)
-        self.loop = asyncio.get_event_loop()
 
     def start_server(self):
         start_server = websockets.serve(self.set_lights, '192.168.0.132', 8080)
 
-        self.loop = asyncio.new_event_loop()
-
-        self.loop.run_until_complete(start_server)
+        asyncio.get_event_loop().run_until_complete(start_server)
         print('Started!\n')
-        self.loop.run_forever()
+        asyncio.get_event_loop().run_forever()
 
 
     def restart(self):
-<<<<<<< HEAD
-<<<<<<< HEAD
-        self.loop.stop()
-        self.loop.close()
-=======
-=======
->>>>>>> parent of c580656... updated stuff
         asyncio.get_event_loop().run_until_complete(asyncio.get_event_loop().shutdown_asyncgens())
         asyncio.get_event_loop().stop()
         asyncio.get_event_loop().close()
 
-<<<<<<< HEAD
->>>>>>> parent of c580656... updated stuff
-=======
->>>>>>> parent of c580656... updated stuff
         start_server = websockets.serve(self.set_lights, '192.168.0.132', 8080)
 
-        self.loop.run_until_complete(start_server)
+        asyncio.get_event_loop().run_until_complete(start_server)
         print('Started!\n')
-        self.loop.run_forever()
+        asyncio.get_event_loop().run_forever()
 
     async def set_lights(self, websocket, path):
-        try:
-            code = await websocket.recv()
-            print(f'Recieved: {code}')
-            if code == 'p3':
-                self.run_preset = True
-                if self.preset_thread != None:
-                    print(self.preset_thread.is_alive())
-                self.preset_thread = threading.Thread(target=self.preset3, name='p3')
-                
-                self.preset_thread.start()
-            else:
-                self.run_preset = False
-                rgb_values = code.split(',')
-                for i, val in enumerate(rgb_values):
-                    rgb_values[i] = int(val)
+        while True:
+            try:
+                code = await websocket.recv()
+                print(f'Recieved: {code}')
+                if code == 'p3':
+                    self.run_preset = True
+                    if self.preset_thread != None:
+                        print(self.preset_thread.is_alive())
+                    self.preset_thread = threading.Thread(target=self.preset3, name='p3')
+                    
+                    self.preset_thread.start()
+                else:
+                    self.run_preset = False
+                    rgb_values = code.split(',')
+                    for i, val in enumerate(rgb_values):
+                        rgb_values[i] = int(val)
 
-                self.pixels.fill((rgb_values[0], rgb_values[1], rgb_values[2]))
-                self.pixels.show()
+                    self.pixels.fill((rgb_values[0], rgb_values[1], rgb_values[2]))
+                    self.pixels.show()
 
-                response = f'The lights have been set to {code}'
-                print(f'Sent: {response}')
-                await websocket.send(response)
-        except websockets.exceptions.ConnectionClosedOK:
-            print('Server Closed')
-            break
-        except websockets.ConnectionClosedError:
-            print('Restarting Server!\n')
-            self.restart()
+                    response = f'The lights have been set to {code}'
+                    print(f'Sent: {response}')
+                    await websocket.send(response)
+            except websockets.exceptions.ConnectionClosedOK:
+                print('Server Closed')
+                break
+            except websockets.ConnectionClosedError:
+                print('Restarting Server!\n')
+                self.restart()
 
     def preset3(self):
         num_leds = 80
